814cfb79e433329e5130b09476cd167d12ab1749
indrasena2210 <indrasena2210@gmail.com>
2023-07-07 07:14:18 +0000

    Congrats Card  app

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div className="background-container">
+    <h1>Congratulations</h1>
+    <div className="congratulations-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran V</h1>
+      <p>Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
+      <div>
+        <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"/>
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
